d5999fd0470b99fdaedf8e378c7c401f167ea664
CongBinh24 <ncb240600@gmail.com>
2025-05-30 21:37:33 +0700

    Animations
    Set Animator

---
+++ Assets/EZ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator _anim;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
